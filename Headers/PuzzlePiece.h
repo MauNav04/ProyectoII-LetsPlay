6f41a56b3e2e032edcb0f62b2276c4741f29e3ac
GabrielVL <gavalo.99@gmail.com>
2021-05-26 13:49:57 -0600

    Created PuzzlePiece with atributes xpos and ypos

---
+++ Headers/PuzzlePiece.h
@@ -0,0 +1,17 @@
+//
+// Created by mavros on 26/5/21.
+//
+
+#ifndef PROYECTOII_LETSPLAY_PUZZLEPIECE_H
+#define PROYECTOII_LETSPLAY_PUZZLEPIECE_H
+
+
+class PuzzlePiece {
+
+private:
+    int xpos;
+    int ypos;
+};
+
+
+#endif //PROYECTOII_LETSPLAY_PUZZLEPIECE_H
---
+++ Sources/PuzzlePiece.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mavros on 26/5/21.
+//
+
+#include "../Headers/PuzzlePiece.h"
