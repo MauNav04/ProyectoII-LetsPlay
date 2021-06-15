5828e194fb76826f68795d3dbabef6b1378f376c
Jose Julian Solano Quesada <juliansolqu18@gmail.com>
2021-06-14 20:48:23 -0600

    Cantidad de bolas para terminar el juego

---
+++ BallWindowSettings/mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_BP_Final_clicked()
+{
+
+}
+
+void MainWindow::on_BP_Final_Back_clicked()
+{
+
+}
